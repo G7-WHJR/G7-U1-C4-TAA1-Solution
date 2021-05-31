0a6e228899aaa76977eef2563c7313c3aa9896e8
G7-WHJR <raveen.anand@whitehatjr.com>
2021-05-31 22:44:47 +0530

    First

---
+++ Variable Conditionals/myscript.js
@@ -0,0 +1,13 @@
+function check()
+{
+    // 1. Declare a variable x, with value 5
+    var x =5;
+
+    // 2. Declare a variable y, with value "Coding"
+    var y ="coding";
+
+   // 3. Check type of the variables using typeOf operator
+   console.log(typeof(x));
+   console.log(typeof(y));
+}
+
